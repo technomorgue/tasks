4de1f5db4af43ba7de7e79624173140c15eb44d5
Libu <lb.mailinglists@gmail.com>
2020-06-30 15:05:26 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+export default class App extends Component {
+	render(){
+		return (
+    			<div className="App">
+				<h1>Hello World</h1>;
+    			</div>
+		);
+	}
+};
